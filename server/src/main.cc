d779cfa9e6d4c842be49c2de326046e20461811e
lrenr <github@pm.luisrenner.de>
2024-08-05 00:32:09 +0200

    tab fix

--- server/src/main.cc
+++ server/src/main.cc
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
 	if (Device::setup_msix(dev, table_offset, bir, table_size)) throw;
 	if (table_size + 1 < 10) throw;
 	/* map BAR that contains the MSI-X table */
-    l4_uint8_t* table_bar;
+	l4_uint8_t* table_bar;
 	if(bir == 0) table_bar = bar0;
 	else table_bar = Device::map_pci_bar(dev, bir);
 	reg32* msix_table = (reg32*)(table_bar + table_offset);
@@ -46,27 +46,27 @@ int main(int argc, char **argv) {
 	MEM::alloc_dma_mem(dma_cap, HCA_PAGE_SIZE * CMD::MBB_MAX_COUNT, &ctx.cmd_args.imb_mem);
 	MEM::alloc_dma_mem(dma_cap, HCA_PAGE_SIZE * CMD::MBB_MAX_COUNT, &ctx.cmd_args.omb_mem);
 
-    ctx.mem_page_pool.max = 10240;
-    ctx.mem_page_pool.size = 0;
-    ctx.mem_page_pool.block_size = 1024;
-    ctx.mem_page_pool.block_count = 0;
-    ctx.mem_page_pool.alloc_block = MEM::alloc_block;
-    ctx.mem_page_pool.free_block = MEM::free_block;
-    ctx.mem_page_pool.data.dma_cap = &dma_cap;
-    ctx.mem_page_pool.start = nullptr;
+	ctx.mem_page_pool.max = 10240;
+	ctx.mem_page_pool.size = 0;
+	ctx.mem_page_pool.block_size = 1024;
+	ctx.mem_page_pool.block_count = 0;
+	ctx.mem_page_pool.alloc_block = MEM::alloc_block;
+	ctx.mem_page_pool.free_block = MEM::free_block;
+	ctx.mem_page_pool.data.dma_cap = &dma_cap;
+	ctx.mem_page_pool.start = nullptr;
 
 	MEM::MEM_Page* mp = MEM::alloc_page(&ctx.mem_page_pool);
 	MEM::free_page(&ctx.mem_page_pool, mp->data.phys);
 
 	ctx.uar_page_pool.max = 1024;
-    ctx.uar_page_pool.size = 0;
-    ctx.uar_page_pool.block_size = 64;
-    ctx.uar_page_pool.block_count = 0;
-    ctx.uar_page_pool.alloc_block = UAR::alloc_block;
-    ctx.uar_page_pool.free_block = UAR::free_block;
-    ctx.uar_page_pool.data.base.index = 4096;
+	ctx.uar_page_pool.size = 0;
+	ctx.uar_page_pool.block_size = 64;
+	ctx.uar_page_pool.block_count = 0;
+	ctx.uar_page_pool.alloc_block = UAR::alloc_block;
+	ctx.uar_page_pool.free_block = UAR::free_block;
+	ctx.uar_page_pool.data.base.index = 4096;
 	ctx.uar_page_pool.data.base.addr = (UAR::Page*)(bar0 + (HCA_PAGE_SIZE * 4096));
-    ctx.uar_page_pool.start = nullptr;
+	ctx.uar_page_pool.start = nullptr;
 
 	UAR::UAR_Page* up = UAR::alloc_page(&ctx.uar_page_pool);
 	UAR::free_page(&ctx.uar_page_pool, up);
@@ -78,11 +78,11 @@ int main(int argc, char **argv) {
 	ctx.cmd_args.dbv = &init_seg->dbv;
 	ctx.cmd_args.cq.size = 32;
 	ctx.cmd_args.cq.start = (CMD::CQE*)ctx.cmd_args.cq.dma_mem.virt;
-	
+
 	printf("------------\n\n");
 	init_hca(ctx, init_seg);
 	printf("pool_size: %llu | pool_block_count %llu\n", ctx.mem_page_pool.size, ctx.mem_page_pool.block_count);
-	
+
 	printf("------------\n\n");
 	//UAR::UAR uar = alloc_uar(ctx, bar0);
 	//setup_event_queue(ctx, icu_src, msix_table, icu, dma_cap, uar);
